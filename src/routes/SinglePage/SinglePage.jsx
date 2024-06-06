978e47a2a101aebff845aefd8ee0471d9309c7f9
agnel <agnelrozario@ymail.com>
2024-06-06 18:27:24 +0400

    new
    new

--- src/routes/SinglePage/SinglePage.jsx
+++ src/routes/SinglePage/SinglePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./singlepage.scss";
 import { singlePostData, userData } from "../../lib/dummydata";
 import Slider from '../../components/Slider/Slider';
+import Map from '../../components/Map/Map';
 
 const SinglePage = () => {
   return (
@@ -32,7 +33,62 @@ const SinglePage = () => {
       </div>
       <div className="features">
         <div className="wrapper">
-
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/bill.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pets Policy</span>
+                <p>Pets are allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/bill.png" alt="" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>80 sqft.</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="" />
+              <span>2 Beds</span>
+            </div>
+            <div className="size">
+              <img src="/bathtub.png" alt="" />
+              <span>1 Bathroom</span>
+            </div>
+          </p>
+          <p className="title">Sizes</p>
+          <div className="sizes"> </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal"></div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="" />
+              Save the Location
+            </button>
+          </div>
         </div>
       </div>
     </div>
